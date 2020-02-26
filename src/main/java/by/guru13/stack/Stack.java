14e0e25b29d45026d3b4a9dabf36726e03320421
guru13 <ovsyanka13@gmail.com>
2020-02-26 21:46:13 +0300

    Refactor

--- src/main/java/by/guru13/stack/Stack.java
+++ src/main/java/by/guru13/stack/Stack.java
@@ -6,21 +6,16 @@ public class Stack<T>
     public ArrayList<T> arrayList;
     public Stack()
     {
-        // инициализация внутреннего хранилища стека
        arrayList = new ArrayList<T>();
     }
 
     public int size()
     {
-        // размер текущего стека
         return arrayList.size();
     }
 
     public T pop()
     {
-        // ваш код
-//        T popValue = arrayList.get(arrayList.size()-1);
-//        arrayList.remove(arrayList.size()-1);
         if(arrayList.size() == 0) {
             return null;
         } else {
@@ -30,18 +25,15 @@ public class Stack<T>
 
     public void push(T val)
     {
-        // ваш код
         arrayList.add(val);
     }
 
     public T peek()
     {
-        // ваш код
         if(arrayList.size() == 0) {
             return null;
         } else {
             return arrayList.get(arrayList.size()-1);
         }
-         // если стек пустой
     }
 }
