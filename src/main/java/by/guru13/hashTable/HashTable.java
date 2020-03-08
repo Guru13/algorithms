63e17729bb8fd404208dade4987a57e9d1821248
guru13 <ovsyanka13@gmail.com>
2020-03-08 15:19:55 +0300

    Fixed find

--- src/main/java/by/guru13/hashTable/HashTable.java
+++ src/main/java/by/guru13/hashTable/HashTable.java
@@ -55,16 +55,16 @@ public class HashTable {
     public int find(String value) {
         // находит индекс слота со значением, или -1
         int idx = hashFun(value);
-        if (slots[idx].equals(value)) return idx;
+        if (slots[idx] != null && slots[idx].equals(value)) return idx;
         int circleCount = slots.length;
-        while (!slots[idx].equals(value) && circleCount != 0) {
+        while ((slots[idx] != null && !slots[idx].equals(value)) && circleCount != 0) {
             idx = idx + step;
             if (idx > slots.length - 1) {
                 idx = slots.length - circleCount;
             }
             circleCount--;
         }
-        if (slots[idx].equals(value)) {
+        if (slots[idx] != null && slots[idx].equals(value)) {
             return idx;
         } else {
             return -1;
